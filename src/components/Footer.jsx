50648687476c5f18b60bfadc5fe22ada2e931f1f
ReZeroKairu <keilanimaynard@gmail.com>
2024-11-04 16:09:03 +0800

    changed text in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-red-800 text-white py-4 text-center">
-      <p>© 2024 Liceo de Cagayan University. All rights reserved.</p>
+      <p>© 2024 PubTrack-Liceo de Cagayan University. All rights reserved.</p>
       {/* Add more footer content here if needed */}
     </footer>
   );
