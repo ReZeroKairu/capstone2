0b6977322fa5b17da0761d937fcc6429fb3a7917
ReZeroKairu <keilanimaynard@gmail.com>
2025-08-19 14:01:52 +0800

    Adjusted home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,15 +10,17 @@ function Home() {
       />
 
       {/* Logo */}
-      <img
-        src="/pubtracklogo.png"
-        alt="PubTrack Logo"
-        className="
-    h-[300px] sm:h-[280px] md:h-[350px] lg:h-[420px] 
-    absolute bottom-[50px] right-[70px] 
-    max-sm:static max-sm:mx-auto max-sm:mt-40 max-sm:h-[250px]
-  "
-      />
+      <div className="max-sm:flex max-sm:flex-1 max-sm:items-center max-sm:justify-center">
+        <img
+          src="/pubtracklogo.png"
+          alt="PubTrack Logo"
+          className="
+      h-[300px] sm:h-[280px] md:h-[350px] lg:h-[420px] 
+      absolute bottom-[50px] right-[70px] 
+      max-sm:relative max-sm:h-[300px] max-sm:ml-36 max-sm:z-10
+    "
+        />
+      </div>
     </div>
   );
 }
