66b57061d6077836eb88e30abaf69d6b97c3a4dd
ReZeroKairu <keilanimaynard@gmail.com>
2025-09-09 23:55:40 +0800

    img feature in announcement and callforpapers

--- src/utils/quillConfig.js
+++ src/utils/quillConfig.js
@@ -1,9 +1,20 @@
+// src/utils/quillConfig.js
+import Quill from "quill";
+
+// Define custom sizes to match your toolbar
+const Size = Quill.import("formats/size");
+Size.whitelist = ["small", false, "large", "huge"];
+Quill.register(Size, true);
+
 export const quillModules = {
   toolbar: [
     [{ header: [1, 2, 3, false] }],
     ["bold", "italic", "underline", "strike"],
     [{ color: [] }, { background: [] }],
-    [{ font: [] }, { size: ["small", false, "large", "huge"] }],
+    [
+      { font: ["serif", "monospace", "sans-serif"] },
+      { size: ["small", false, "large", "huge"] }, // now huge works
+    ],
     [{ list: "ordered" }, { list: "bullet" }],
     [{ align: [] }],
     ["blockquote", "code-block"],
